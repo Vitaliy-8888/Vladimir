902aa5f86692b8b3d7aa776de216087a2ebd4835
Vitaliy <vladimir42142@mail.ru>
2022-07-09 23:53:02 +0300

    Случайные числа в 003

--- Exemples003/Program.cs
+++ Exemples003/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 6;
-int numberB = 8;
+﻿int numberA = new Random().Next(1, 10); // 1, 2, 3 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.Write("Сумма = ");
 Console.WriteLine(result);  
