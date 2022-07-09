eb9e647032487bf8e6aea4437ce926973c70afe8
Vitaliy <vladimir42142@mail.ru>
2022-07-09 21:09:43 +0300

    Изменили Examples002

---
+++ Examples002/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите ваше имя и нажмите Enter ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);  
---
+++ Exemples003/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = 6;
+int numberB = 8;
+int result = numberA + numberB;
+Console.Write("Сумма = ");
+Console.WriteLine(result); 
